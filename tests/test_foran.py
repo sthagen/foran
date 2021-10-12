6c4ccf39651d740d05b076f85211b0b222d639c8
Stefan Hagen <stefan@hagen.link>
2021-10-12 18:57:09 +0200

    Added test to bring test coverage again above 4/5

--- tests/test_foran.py
+++ tests/test_foran.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,unused-import,reimported
+from git import Repo
+
 import foran.foran as fb
+from foran.status import Status
 
 
 def test_main():
     assert fb.main(['foran-eller-bagved.functional']) == 0
+
+
+def test_local_commits():
+    repo = Repo('.')
+    status = Status(repo)
+    status.foran = False
+    fb.local_commits(repo, status)
+    assert status.local_commits == []
