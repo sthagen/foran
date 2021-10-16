7504f639185a52d90f890dd94d7cfce94d81fc92
Stefan Hagen <stefan@hagen.link>
2021-10-16 19:27:55 +0200

    Added tests for CLI but CLI grew faster

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,40 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring,unused-import,reimported
+import pytest
+
 import foran.cli as cli
 
 
-def test_cli():
-    assert cli.main(['foran-eller-bagved.txt']) == 0
+def test_cli_main():
+    message = 'ignoring template: ignored'
+    with pytest.raises(UserWarning) as ex:
+        cli.main(['diff', '.', 'foran-eller-bagved.txt', 'ignored']) == 0
+        assert message in str(ex.value)
+
+
+def test_cli_diff_path():
+    message = 'ignoring template: ignored'
+    with pytest.raises(UserWarning) as ex:
+        cli.diff_repo(source=cli.CWD, output='foran-eller-bagved.txt') == 0
+        assert message in str(ex.value)
+
+
+def test_cli_diff_stdout():
+    message = 'ignoring template: ignored'
+    with pytest.raises(UserWarning) as ex:
+        cli.diff_repo(source=cli.CWD) == 0
+        assert message in str(ex.value)
+
+
+def test_cli_label_path():
+    message = 'ignoring template: ignored'
+    with pytest.raises(UserWarning) as ex:
+        cli.label_repo(source=cli.CWD, output='foran-eller-bagved.txt') == 0
+        assert message in str(ex.value)
+
+
+def test_cli_label_stdout():
+    message = 'ignoring template: ignored'
+    with pytest.raises(UserWarning) as ex:
+        cli.label_repo(source=cli.CWD) == 0
+        assert message in str(ex.value)
