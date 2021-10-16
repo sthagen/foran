8be0be878b07a08fc9721019f51d07eeaf53493a
Stefan Hagen <stefan@hagen.link>
2021-10-16 19:20:51 +0200

    Switched to typer and placed magic into default main

--- foran/__main__.py
+++ foran/__main__.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-docstring
 import sys
 
-from foran.cli import main
+from foran.cli import app
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))  # pragma: no cover
+    sys.exit(app(prog_name='foran'))  # pragma: no cover
