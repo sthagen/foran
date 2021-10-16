b493f8ab12dcd97d1b6613db3c1b4d93bef4f894
Stefan Hagen <stefan@hagen.link>
2021-10-16 19:25:26 +0200

    Changed entry pointinto functional module to new separation of work

--- foran/foran.py
+++ foran/foran.py
@@ -2,7 +2,6 @@
 # pylint: disable=expression-not-assigned,line-too-long
 """In front or behind (Foran eller bagved)? API."""
 import os
-import sys
 import warnings
 from typing import List, Union
 
@@ -27,7 +26,7 @@ def local_commits(repo: Repo, status: Status) -> None:
             status.local_commits = [rev for rev in repo.iter_commits(f'origin/{branch}..{branch}')]
         except GitCommandError as ex:
             symptom = ex.stderr.replace('\n', '')
-            if "fatal: bad revision 'origin/default..default" in symptom:
+            if "fatal: bad revision 'origin/default..default'" in symptom:
                 warning = 'No remote found, so all commit differences hypothetical'
                 status.local_commits = [warning]
                 warnings.warn(warning)
@@ -47,12 +46,24 @@ def local_files(repo: Repo, status: Status) -> None:
 
 def main(argv: Union[List[str], None] = None) -> int:
     """Drive the analysis."""
-    argv = argv if argv else sys.argv[1:]
-    report = Report() if not argv else Report(stem=argv[0], file_format=Format.NONE)
-    repo = Repo('.')
+    # [('diff'|'label'), repo_root, target, template]
+    if not argv or len(argv) != 4:
+        warnings.warn('received wrong number of arguments')
+        return 2
+
+    if argv[0] not in ('diff', 'label'):
+        warnings.warn(f'received unknown command: {argv[0]}')
+        return 2
+
+    command, repo_root, target, template = argv
+    report = Report(stem=target, file_format=Format.NONE)
+    if target != 'STD_OUT':
+        warnings.warn(f'ignoring template: {template}')
+    repo = Repo(repo_root)
     status = Status(repo)
-    local_commits(repo, status)
-    local_staged(repo, status)
-    local_files(repo, status)
+    if command == 'diff':
+        local_commits(repo, status)
+        local_staged(repo, status)
+        local_files(repo, status)
     report_as(status, report)
     return 0
