fbc89d37125474449706733a4fea816c6291c2b5
Stefan Hagen <stefan@hagen.link>
2022-12-07 12:13:03 +0100

    Bumped impl version for release 2022.12.7

--- foran/__init__.py
+++ foran/__init__.py
@@ -1,8 +1,8 @@
 """In front or behind (Foran eller bagved)? version and init interface."""
 
 # [[[fill git_describe()]]]
-__version__ = '2022.12.6+parent.e68424eb'
-# [[[end]]] (checksum: 42b491a202e452d0d220e3c599c4d341)
+__version__ = '2022.12.7+parent.43ecfe81'
+# [[[end]]] (checksum: 71289b2b25248cb6f3d56c43f8d38504)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
