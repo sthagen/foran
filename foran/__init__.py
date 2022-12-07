6d514994ca2ebe11a8351ec5835d91a8d6381620
Stefan Hagen <stefan@hagen.link>
2022-12-07 12:10:34 +0100

    Injected CP coding like version tag (invalid value still)

--- foran/__init__.py
+++ foran/__init__.py
@@ -1,2 +1,8 @@
 """In front or behind (Foran eller bagved)? version and init interface."""
-__version__ = '2022.12.7'
+
+# [[[fill git_describe()]]]
+__version__ = '2022.12.6+parent.e68424eb'
+# [[[end]]] (checksum: 42b491a202e452d0d220e3c599c4d341)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
