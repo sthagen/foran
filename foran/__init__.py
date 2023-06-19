b393c2d4fd1c1f9bd237e4e5fd0a50b561244ff1
Stefan Hagen <stefan@hagen.link>
2023-06-19 23:33:25 +0200

    Bumped impl version for release 2023.6.19

--- foran/__init__.py
+++ foran/__init__.py
@@ -1,8 +1,8 @@
 """In front or behind (Foran eller bagved)? version and init interface."""
 
 # [[[fill git_describe()]]]
-__version__ = '2022.12.7+parent.43ecfe81'
-# [[[end]]] (checksum: 71289b2b25248cb6f3d56c43f8d38504)
+__version__ = '2023.6.19+parent.ceabb81a'
+# [[[end]]] (checksum: d8b05f53d76c29819c0a875977b220a3)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
